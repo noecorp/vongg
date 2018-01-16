eeabef5bcdd2cac87e3750521d3cb85653c31040
Mateusz Domurad <vimparek@gmail.com>
2018-01-16 21:09:50 +0100

    0.3.4
    Added Single News Styles and HTML

--- backend/models/ajax.class.php
+++ backend/models/ajax.class.php
@@ -1,6 +1,6 @@
 <?php
 
-  class NotFoundModel extends Model {
+  class AjaxModel extends Model {
 
     function __construct() {
 
---
+++ backend/models/news.class.php
@@ -0,0 +1,15 @@
+<?php
+
+  class NewsModel extends Model {
+
+    function __construct() {
+
+      parent::__construct();
+
+    }
+
+    public function showSingleNews($link) {
+      
+    }
+
+  }
